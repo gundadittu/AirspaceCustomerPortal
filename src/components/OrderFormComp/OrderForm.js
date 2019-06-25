aea29566adee5e3adf06d376ad88a34a4947eedb
Aditya Gunda <adityagunda@Adityas-MacBook-Pro.local>
2019-06-25 00:20:59 -0700

    added coffee questions

--- src/components/OrderFormComp/OrderForm.js
+++ src/components/OrderFormComp/OrderForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {
-    Modal, Form, Input, Radio, InputNumber
+    Modal, Form, Input, Radio, InputNumber, Select, Cascader
 } from 'antd';
 import * as generalActionCreator from '../../store/actions/general';
+const { Option } = Select;
 
 class OrderForm extends React.Component {
 
@@ -46,19 +47,141 @@ class OrderForm extends React.Component {
         if (serviceTitle === null) {
             return [];
         } else if (serviceTitle === "Coffee + Tea") {
+
+            const powerSourceChildren = [
+                {
+                    value: 'My office does have a 110V or 120V power source.',
+                    label: 'My office does have a 110V or 120V power source.',
+                },
+                {
+                    value: 'My office does NOT have a 110V or 120V power source.',
+                    label: 'My office does NOT have a 110V or 120V power source.',
+                },
+                {
+                    value: 'I do know whether my office has a 110V or 120V power source.',
+                    label: 'I do know whether my office has a 110V or 120V power source.',
+                }
+            ];
+
+            const waterLineChildren = [
+                {
+                    value: 'My office does have a water line.',
+                    label: 'My office does have a water line.',
+                    children: powerSourceChildren,
+                },
+                {
+                    value: 'My office does NOT have a water line.',
+                    label: 'My office does NOT have a water line.',
+                    children: powerSourceChildren,
+                },
+                {
+                    value: 'I am not sure whether my office has a water line.',
+                    label: 'I am not sure whether my office has a water line.',
+                    children: powerSourceChildren,
+                },
+            ];
+
             return [
-                // { 
-                // const type = x.type || null;
-                // const key = x.key || null;
-                // const question = x.question || null;
-                // }
+                {
+                    type: "cascader",
+                    key: "coffeeType",
+                    question: "Coffee Type?",
+                    message: "Please tell us what type of coffee you're interested in.",
+                    required: true,
+                    data: {
+                        options: [
+                            {
+                                value: 'Fresh Brew',
+                                label: 'Fresh Brew',
+                                children: [
+                                    {
+                                        value: 'Include Free Grind Service',
+                                        label: 'Include Free Grind Service',
+                                    },
+                                    {
+                                        value: 'Do NOT include Free Grind Service',
+                                        label: 'Do NOT include Free Grind Service',
+                                    },
+                                ],
+                            },
+                            {
+                                value: 'Single Serve',
+                                label: 'Single Serve',
+                            },
+                            {
+                                value: 'Bean-to-Cup',
+                                label: 'Bean-to-Cup',
+                            }, 
+                            {
+                                value: 'None',
+                                label: 'None',
+                            }
+                        ]
+                    }
+                },
+                {
+                    type: "select",
+                    key: "coffeePackage",
+                    question: "Coffee Package?",
+                    message: "Please tell us what package you're interested in.",
+                    required: true,
+                    data: {
+                        options: ["Premium", "Standard", "Budget", "None"]
+                    }
+                },
                 {
                     type: "text",
-                    key: "peopleCount",
-                    question: "How many people?",
-                    message: "Please tell us how many people you need coffee for.",
-                    required: true
-                }
+                    key: "coffeeLocation",
+                    question: "Please provide a specific location where the coffee will be served.",
+                    message: "Please provide a specific location where the coffee will be served.",
+                    required: false
+                },
+                {
+                    type: "cascader",
+                    key: "equipmentInstall",
+                    question: "Add-on: Do you need to purchase or rent equipment?",
+                    message: "Please tell us whether your office needs to purchase or rent equipment.",
+                    required: true,
+                    data: {
+                        options: [
+                            {
+                                value: "Interested in Purchasing Equipment",
+                                label: "Interested in Purchasing Equipment",
+                                children: waterLineChildren
+                            },
+                            {
+                                value: "Interested in Renting Equipment",
+                                label: "Interested in Renting Equipment",
+                                children: waterLineChildren
+                            },
+                            {
+                                value: "Do not need equipment",
+                                label: "Do not need equipment",
+                                children: waterLineChildren
+                            }
+                        ]
+                    }
+                },
+                {
+                    type: "select",
+                    key: "teaType",
+                    question: "Tea Type?",
+                    message: "Please tell us what type of tea you're interested in.",
+                    required: true,
+                    data: {
+                        options: ["Sachet", "Single Service", "None"]
+                    }
+                },
+                {
+                    type: "select",
+                    key: "teaPackage",
+                    question: "Tea Package?",
+                    message: "Please tell us which package you're interested in.",
+                    required: true,
+                    data: {
+                        options: ["Premium", "Standard", "Budget", "None"]
+                    }
+                },
             ];
         } else {
             return [];
@@ -116,6 +239,42 @@ class OrderForm extends React.Component {
                                             rules: [{ required: required, whitespace: true, message: message }],
                                         })(
                                             <InputNumber min={0} disabled={confirmLoading} />
+                                        )}
+                                    </Form.Item>
+                                );
+                            } else if (type === "select") {
+                                const options = data.options || [];
+
+                                return (
+                                    <Form.Item label={question}>
+                                        {getFieldDecorator(key, {
+                                            rules: [{ required: required, whitespace: true, message: message }],
+                                        })(
+
+                                            <Select
+                                                showSearch
+                                                // style={{ width: 200 }}
+                                                placeholder={message}
+                                                optionFilterProp="children"
+                                                filterOption={(input, option) =>
+                                                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                                }
+                                            >
+                                                {options.map(x => (
+                                                    <Option value={x}>{x}</Option>
+                                                ))}
+                                            </Select>
+                                        )}
+                                    </Form.Item>
+                                );
+                            } else if (type === "cascader") {
+                                const options = data.options || [];
+                                return (
+                                    <Form.Item label={question}>
+                                        {getFieldDecorator(key, {
+                                            rules: [{ required: required, message: message }],
+                                        })(
+                                            <Cascader options={options} placeholder={message} />
                                         )}
                                     </Form.Item>
                                 );
